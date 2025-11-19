20916f9035e4607e04299f1e7fe156e6b96d33b7
DDDBBBPPP <david.bellon.payer@gmail.com>
2025-11-19 10:51:06 +0100

    Pa casa

---
+++ src/tema07/clases/Usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+
+    class Usuario
+    {
+
+
+       private int $idUsuario;
+       private string $nombre;
+       private string $apellido;
+       private string $dni;
+       private string $email;
+       private string $password;
+
+
+
+    }
--- src/tema07/index.php
+++ src/tema07/index.php
@@ -1,5 +1,6 @@
 <?php
-#Conectamos con la base de datos utilizando PDO
+    #Conectamos con la base de datos utilizando PDO
+    require_once "./clases/Usuario.php";
 
     try {
 
@@ -8,7 +9,7 @@
 
         $pdo = PDO::connect($dns, "root", "root"); // Devuelve un objeto de clase PDO \mysql
 
-    } catch(PDOException $pdoe){
+    } catch (PDOException $pdoe) {
         die ("**ERROR: " . $pdoe->getMessage());
     }
 
@@ -16,7 +17,32 @@
 
     # Devuelve un objeto de tipo PDOStatement: implementa una interfaz Traversable
     #La interfaz convierte el objeto en iterable (se puede recorrer con foreach)
-    $resultado =$pdo->query("SELECT * FROM usuario ;");
+    $resultado = $pdo->query("SELECT * FROM usuario;"); // El query para consultas de tipo select.
+
+    #Recuperando inforamcion
+    #1 Objeto PDOStatement que es itreable
+    #2 Méto do fetchAll() de la clase PDOStatement devuelve un array con todas las filas
+    //$datos = $resultado->fetchAll();  //Si pones PDO::FETCH_NUM o PDO::FETCH_ASSOC pues te da un array numerico y dentro
+    // o un array escalar o uno asociativo Array de arrays asociativos
+    // SI pones dentro del fetchAll(PDO::FETCH_COLUMN,1 o 2 o 3) te da la columna en un array escalar. Por ejemplo todos los DNIS
+    //FETCH_OBJ te devuelve un array de objetos
+    //echo  $datos[0]->nombre
+
+    // OTRA es PDO::FETCH_CLASS, "Usuario"), Puedes poner tambien en vez
+    //de "Usuario" Usuario::class y te devuelve un array de objetos de la clase Usuario
+    // Si no pònes constante seria PDO::FETCH_BOTH que es un array mixto
+
+
+
+     # 3 metodo fetch() de la clase PDOStatement devuelve una fila cada vez que se llama ( Si no pones nada, con el both,
+    // Y si no alguno mas, te deja
+
+
+    #4 fetchObject() de la clase PDOStatement devuelve una fila cada vez que se llama como un objeto estandar
+    // o de una clase concreta si se lo indicas
+
+    #5 fetchColumn() de la clase PDOStatement devuelve el valor de una columna concreta de la fila actual.
+
 ?>
 
 <!doctype html>
@@ -26,12 +52,12 @@
     <title>Bases de Datos</title>
 </head>
 <body>
-    <div class="container">
-        <ul>
+<div class="container">
+    <ul>
         <?php foreach ($resultado as $usuario): ?>
-            <li><?php echo "{$usuario["nombre"]} {$usuario["apellido"]}"?></li>
+            <li><?php echo "{$usuario["nombre"]} {$usuario["apellido"]}" ?></li>
         <?php endforeach; ?>
         <ul/>
-    </div>
+</div>
 </body>
 </html>
